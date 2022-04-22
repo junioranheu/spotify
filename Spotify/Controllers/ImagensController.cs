f1538d8d147affab6c0877291d45e92f8cd679d0
junioranheu <junioranheu@gmail.com>
2022-04-22 01:32:56 -0300

    criar end-point Imagens e método "GetBase64PorCaminhoId"

---
+++ Spotify/Controllers/ImagensController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spotify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagensController : BaseController<ImagensController>
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ImagensController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("getBase64PorCaminhoId")]
+        public string GetBase64PorCaminhoId(string caminho)
+        {
+            // Parâmetro "caminho" = "capas/22.webp";
+            string contentRootPath = _hostingEnvironment.ContentRootPath; // Vai até a raiz;
+            string caminhoDestino = contentRootPath + "Upload/" + caminho; // Caminho de destino;
+
+            if (!System.IO.File.Exists(caminhoDestino))
+            {
+                return "";
+            }
+
+            byte[] imageArray = System.IO.File.ReadAllBytes(caminhoDestino);
+            string base64 = Convert.ToBase64String(imageArray);
+
+            return base64;
+        }
+    }
+}
